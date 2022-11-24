60b7db6cbc498b036739e2cac916fccdd1cf2af2
Mehtap Tataroğlu <teytap@gmail.com>
2022-11-24 19:31:19 +0300

    added copy function

--- App.js
+++ App.js
@@ -94,14 +94,30 @@ const characters = [
 
 let passwordEl1 = document.getElementById("password1");
 let passwordEl2 = document.getElementById("password2");
+let copyEl = document.getElementById("copy");
+let copyEl2 = document.getElementById("copy2");
 
 function passwordGenerator() {
-  passwordEl1.textContent = "";
-  passwordEl2.textContent = "";
+  passwordEl1.value = "";
+  passwordEl2.value = "";
+  copyEl.style.color = "white";
+  copyEl2.style.color = "white";
   for (let i = 0; i < 15; i++) {
     let char1 = characters[Math.floor(Math.random() * characters.length)];
     let char2 = characters[Math.floor(Math.random() * characters.length)];
-    passwordEl1.textContent += char1;
-    passwordEl2.textContent += char2;
+    passwordEl1.value += char1;
+    passwordEl2.value += char2;
   }
+}
+function copyToClipboard() {
+  alert(passwordEl1);
+}
+document.querySelector("#copy").addEventListener("click", copy);
+document.querySelector("#copy2").addEventListener("click", copy);
+
+function copy() {
+  var copyText = document.getElementById("password1").value;
+  navigator.clipboard.writeText(copyText).then(() => {
+    alert("Copied to clipboard");
+  });
 }
